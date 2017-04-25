37da4e7edaddcd44d75b9b0f66a0669e1d35df53
Adam Rudny <rudnyadam@gmail.com>
2017-04-25 20:07:47 +0200

    added functionality html + js

--- zadanie-1/scripts.js
+++ zadanie-1/scripts.js
@@ -1,10 +1,21 @@
-var a = prompt("Wpisz podstawę trójkąta: "),
-	h = prompt("Wpisz wysokość trójkąta: "),
-	mNumberOne = prompt("Cyfra do mnożenia I: "),
-	mNumberTwo = prompt("Cyfra do mnożenia II: "),
-	triangleArea = a*h/2,
-	multiplication = mNumberOne*mNumberTwo;
+function getTriangleArea (a, h) {
+	if (a <= 0, h <= 0) {
+		return "Nieprawidłowe dane"
+	}
+	else if (a >= 0, h >= 0) {
+		return a * h / 2;
+	}
+}
 
-console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + "!");
-console.log("Wynik mnożenia to: " + multiplication);
-alert("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea + ", a wynik mnożenia wynosi: " + multiplication);
+console.log(getTriangleArea(10, 6)); // 30
+
+var triangleArea1 = getTriangleArea(10, 50); // 250
+var triangleArea2 = getTriangleArea(50, triangleArea1); // 6250
+var triangleArea3 = getTriangleArea(triangleArea2, 666); // 2081250
+
+console.log(triangleArea1, triangleArea2, triangleArea3);
+
+var userTriangleArea = getTriangleArea(prompt("Podaj podstawę trójkąta:"), prompt("Podaj wysokość trójkąta:")); // User input calc
+
+console.log("Twoje pole trójkąta wynosi: " + userTriangleArea);
+alert("Twoje pole trójkąta wynosi: " + userTriangleArea);
