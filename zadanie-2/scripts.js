17ed8fb6d9a4916d8f1547a0db256d2974b6a942
Adam Rudny <rudnyadam@gmail.com>
2017-04-25 21:03:48 +0200

    added html and js

---
+++ zadanie-2/scripts.js
@@ -0,0 +1,5 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = concat(femaleNames + maleNames);
+console.log(allNames);
