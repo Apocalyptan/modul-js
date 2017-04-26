8a0d75aaf769a9e75a5d11895003a49e1fa57c0f
Adam Rudny <rudnyadam@gmail.com>
2017-04-26 17:00:46 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach."
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+var animal = "Zielone słonie";
+var animalUpperCased = animal.toUpperCase();
+
+
+var textAfter = text.replace("Papugi", animalUpperCased);
+
+console.log(textAfter);
+
+var partOfTextAfter = textAfter.slice(0, (textAfter.length / 2));
+
+console.log(partOfTextAfter);
